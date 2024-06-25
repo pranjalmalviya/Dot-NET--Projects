ba7b04abd947fff14749980179d6ecefcea905b2
Pranjal malviya <71810077+pranjalmalviya@users.noreply.github.com>
2024-06-25 12:51:52 +0530

    Assingment 6 Existing_Assingment_Changes
     Employee Management System Existing Project Changes

---
+++ EmployeeManagementSystem/EmployeeManagementSystem/CosmoDB/ICosmoDBService.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeManagementSystem.Entities;
+
+namespace EmployeeManagementSystem.CosmoDB
+{
+    public interface ICosmoDBService
+    {
+        //Generic Function for all services
+        Task<T> Add<T>(T entity);
+        Task<T> Update<T>(string id,T entity);
+
+        //Additional Details Function
+        Task DeleteAdditionalDetails(string id);
+        Task<EmployeeAdditionalDetailsEntity> GetEmployeeAdditionalDetailsById(string id);
+        Task<IEnumerable<EmployeeAdditionalDetailsEntity>> GetAllAdditionalDetails();
+
+
+        //Baisc Details Function
+        Task<EmployeeBasicDetailsEntity> GetEmployeeBasicDetailsById(string id);
+        Task DeleteBasicDetails(string id);
+        Task<IEnumerable<EmployeeBasicDetailsEntity>> GetAllBasicDetails();
+
+    }
+}
---
+++ EmployeeManagementSystem/EmployeeManagementSystem/Interface/IEmployeeBasicDetailsService.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeManagementSystem.DTO;
+using EmployeeManagementSystem.Entities;
+
+namespace EmployeeManagementSystem.Interface
+{
+    public interface IEmployeeBasicDetailsService
+    {
+        Task<EmployeeBasicDetailsDTO> AddEmployeeBasicDetails(EmployeeBasicDetailsDTO employeeBasicDetails);
+        Task<IEnumerable<EmployeeBasicDetailsDTO>> GetAllEmployeeBasicDetails();
+        Task<EmployeeBasicDetailsDTO> GetEmployeeBasicDetailsById(string id);
+        Task<EmployeeBasicDetailsDTO> UpdateEmployeeBasicDetails(string id, EmployeeBasicDetailsDTO employeeBasicDetails);
+        Task DeleteEmployeeBasicDetails(string id);
+        Task<EmployeeFilterCriteria> GetAllEmployeesByPagination(EmployeeFilterCriteria employeeFilterCriteria);
+
+        Task<VisitorDTO> AddVisitorByMakePostRequest(VisitorDTO visitor);
+        Task<IEnumerable<VisitorDTO>> GetVisitorByMakePostRequest();
+
+    }
+}
