393f16ee77cb16ebecaa1fea04c2412d1239f97e
Pranjal malviya <71810077+pranjalmalviya@users.noreply.github.com>
2024-06-25 12:45:34 +0530

    Assingment_6 Updated_existing_Assingment
    EmployeeManagementSystemUpdated

---
+++ EmployeeManagementSystem/EmployeeManagementSystem/Common/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using EmployeeManagementSystem.DTO;
+using EmployeeManagementSystem.Entities;
+
+namespace EmployeeManagementSystem.Common
+{
+    public class AutoMapperProfile:Profile
+    {
+        public AutoMapperProfile() {
+            CreateMap<EmployeeBasicDetailsDTO,EmployeeBasicDetailsEntity>().ReverseMap();
+            CreateMap<EmployeeAdditionalDetailsDTO,EmployeeAdditionalDetailsEntity>().ReverseMap();
+        }
+    }
+}
